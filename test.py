8f2484955410aec24a6ace68b32cc53ac8b8ded1
Mingee KIm <uilove216@gmail.com>
2024-07-10 11:55:35 +0900

    add python 2024.07.10
    .

--- test.py
+++ test.py
@@ -1,21 +1,25 @@
-def solution(n):
-    str_n = str(n)
-    new_number = 0
+zip_number = input()
+zip_number_list = list(zip_number)
+zip_number_list.reverse()
+answer_numbers = ""
+ex_numbers = ""
+
+situation = False # False 시 붙이기
+# situation = True # True 시 ex_numbers 여러번 붙이기
+
+for i in zip_number_list:
+    if i == ")":
+        continue
+
+    if i == "(":
+        ex_numbers = answer_numbers
+        situation = True
+    else:
+        if situation:
+            answer_numbers = ex_numbers * int(i)
+            situation = False
+        else :
+            answer_numbers = i + answer_numbers
 
 
-    for idx, i in enumerate(str_n):
-        else_list = sorted(list(str_n[idx+1:]))
-        if int("".join(else_list[::-1])) == int(str_n[idx+1:]):
-            print(idx)
-            for idx2, j in enumerate(else_list):
-                if j > i:
-                    break
-
-    
-            new_number = else_list[idx2]
-            else_list[idx2] = i
-            break
-
-    return str_n[:idx] + new_number + "".join(else_list)
-
-print(solution(364))
+print(len(answer_numbers))
