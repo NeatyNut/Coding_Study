94390b96e9c9ec745867234933a66023f925ee2b
Mingee KIm <uilove216@gmail.com>
2024-08-26 23:33:52 +0900

    add python 2024.08.26

--- test.py
+++ test.py
@@ -1,9 +1,2 @@
-input_number = input().split(" ")
-number = int(input_number[0])
-count = int(input_number[1])
-
-number += 1
-while len(set(str(number))) != count:
-    number += 1
-
-print(number)
+x, y = map(int, (input().split()))
+print(x, y)
